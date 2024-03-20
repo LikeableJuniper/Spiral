e5fcfff6b00416da90d79f56a25dadb0956f0e09
Noa Steffen <noasteffen07@gmail.com>
2024-03-20 06:54:16 +0100

    .

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # This program was created to solve the problem depicted in problem.png.
 # The goal is to calculate the coordinates of point A if the length of each segment is a gemoetrical series with q = 2/3 and a_1 = 39.
 # Me and another pupil tried solving this problem using linear maps with 2x2 matrices but did not succeed, as the vectors cannot be represented as linear maps of each other.
-# Our next thought was to expand this problem to 4 dimensions, using a 4x4 matrix and vector, where the first two components of the vector would result in two-dimensional x when added and the other two in two-dimensional y.
+# Our next thought was to expand this problem to 4 dimensions, using a 12x12 matrix and vector, where the first four components of the vector would result in two-dimensional x when added and the other four in two-dimensional y.
+# The last four would be used to calculate the growth of the path length so one could subtract it from coordinates where needed.
 # This would allow for more intricate manipulation of the coordinates with each iteration and might lead to a solution where using matrices could be viable.
 #################################################################
 
